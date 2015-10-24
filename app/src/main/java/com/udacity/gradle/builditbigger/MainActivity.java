fc4c4ef951908c6372c7516ad3442f3c695579e2
smasini <simone.masini92@gmail.com>
2015-10-24 15:25:34 +0200

    step 5

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +39,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        /*Joker joker = new Joker();
-        Intent intent = new Intent(this, JokerActivity.class);
-        intent.putExtra(getString(R.string.extra_joke), joker.getJoke());
-        startActivity(intent);*/
-
         new EndpointsAsyncTask(this).execute();
     }
 
